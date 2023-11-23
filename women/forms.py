26e4af438930deef42ea8374511bf442039a2b41
mista-ai <mr.bezdar@gmail.com>
2023-11-23 09:19:10 +0300

    forms validations

---
+++ women/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.utils.deconstruct import deconstructible
+
+from .models import Category, Husband
+
+
+@deconstructible
+class RussianValidator:
+    ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
+    code = 'russian'
+
+    def __init__(self, message=None):
+        self.message = message if message else 'Должны присутствовать только русские символы, дефис и пробел'
+
+    def __call__(self, value, *args, **kwargs):
+        if not (set(value) <= set(self.ALLOWED_CHARS)):
+            raise forms.ValidationError(self.message, code=self.code)
+
+
+class AddPostForm(forms.Form):
+    title = forms.CharField(max_length=255, min_length=5, label='Заголовок',
+                            widget=forms.TextInput(attrs={'class': 'form-input'}),
+                            error_messages={
+                                'min_length': 'Слишком короткий заголовок',
+                                'required': 'Без заголовка никак'
+                            }, )
+    slug = forms.SlugField(max_length=255, label='URL',
+                           validators=[
+                               MinLengthValidator(5, message='Минимум 5 символов'),
+                               MaxLengthValidator(100, message='Максимум 100 символов'),
+                           ])
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Контент')
+    is_published = forms.BooleanField(required=False, label='Статус', initial=True)
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Категория не выбрана')
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False, label='Муж',
+                                     empty_label='Не замужем')
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
+
+        if not (set(title) <= set(ALLOWED_CHARS)):
+            raise forms.ValidationError('Должны присутствовать только русские символы, дефис и пробел')
--- women/models.py
+++ women/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
@@ -24,7 +25,11 @@ class Women(models.Model):
         PUBLISHED = 1, 'Опубликовано'
 
     title = models.CharField(max_length=255, verbose_name='Заголовок')
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='Slug',
+                            validators=[
+                                MinLengthValidator(5, message='Минимум 5 символов'),
+                                MaxLengthValidator(100, message='Максимум 100 символов'),
+                            ])
     content = models.TextField(blank=True, verbose_name='Текст статьи')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
